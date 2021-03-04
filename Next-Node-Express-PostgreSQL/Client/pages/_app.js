7d8cf6f5a09a47018fe52cbd4b49654875c6867f
pellul <49030609+raven99p@users.noreply.github.com>
2021-03-04 16:13:47 +0200

    Add files via upload

---
+++ Next-Node-Express-PostgreSQL/Client/pages/_app.js
@@ -0,0 +1,8 @@
+import 'antd/dist/antd.css'
+import '../styles/vars.css'
+import '../styles/global.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
