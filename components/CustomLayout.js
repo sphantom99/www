f04338448c1b15f1d9c02fc3ba425923a9543162
pellul <paulinhoellul@gmail.com>
2021-07-04 19:39:58 +0300

    Added login page

---
+++ components/CustomLayout.js
@@ -0,0 +1,22 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
+import React from 'react';
+
+const { Header, Content, Footer } = Layout;
+
+export default function CustomLayout({ children }) {
+  return (
+    <Layout className="layout">
+      <Header>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item>Profile</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <div className="site-layout-content">{children}</div>
+        </Breadcrumb>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>WEB ©2021 Created by CEID students</Footer>
+    </Layout>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import 'antd/dist/antd.css'
-import '../styles/vars.css'
-import '../styles/global.css'
+/* eslint-disable react/jsx-props-no-spreading */
+import 'antd/dist/antd.css';
+import '../styles/vars.css';
+import '../styles/global.css';
+import React from 'react';
+import CustomLayout from '../components/CustomLayout';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <CustomLayout>
+      <Component {...pageProps} />
+    </CustomLayout>
+  );
 }
