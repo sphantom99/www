6e9923b52869632c312fc681d67975d62ff33702
pellul <paulinhoellul@gmail.com>
2021-07-05 16:13:00 +0300

    Finished login screen

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Form, Input, Button, Checkbox, Row, Col,
+  Form, Input, Button, Row, Col,
 } from 'antd';
 
 export default function Login() {
@@ -13,46 +13,43 @@ export default function Login() {
   };
 
   return (
-    <Row>
-      <Col xs={24} />
-      <Col xs={8} />
-      <Col xs={8}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+    <>
+      <Row type="flex" justify="center" align="middle">
+        <Col>
+          <h2>Login here</h2>
+        </Col>
+      </Row>
+      <Row type="flex" justify="center" align="middle">
+        <Col>
+          <Form
+            name="basic"
+            wrapperCol={{ span: 35 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              name="username"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input placeholder="Username" />
+            </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
 
-          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-      <Col xs={8} />
-    </Row>
+            <Form.Item wrapperCol={{ offset: 7, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </>
   );
 }
