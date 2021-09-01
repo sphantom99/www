3c20a2c339b0cd47605ff3be4cfc44e1df2a25aa
pellul <paulinhoellul@gmail.com>
2021-09-01 14:11:11 +0300

    Added logo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-// Custom DatePicker that uses Day.js instead of Moment.js
-
-import { SmileFilled } from '@ant-design/icons';
-
+import Image from 'next/image';
 import Link from 'next/link';
+import { Typography } from 'antd';
 
+const { Title } = Typography;
 const content = {
   marginTop: '100px',
 };
@@ -13,14 +12,20 @@ export default function Home() {
   return (
     <div style={content}>
       <div className="text-center mb-5">
+        <Title>Welcome to</Title>
         <Link href="/login">
-          <a href="/login">
-            <SmileFilled size={48} strokeWidth={1} />
-          </a>
+          <h1>
+            <a href="/login">Press Here to procced</a>
+          </h1>
         </Link>
-
-        <p className="mb-0 mt-3 text-disabled">Welcome to HARHUB !</p>
       </div>
+      <Image
+        src="/HarHub-logos2.jpeg"
+        className="logo"
+        height="1000"
+        width="1000"
+        alt="Picture of the author"
+      />
     </div>
   );
 }
